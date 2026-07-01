5d9bac6164ec8fe1deb8747996d444fa6d2fc4ff
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-01 22:38:51 +0400

    Print the output

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,3 +3,11 @@ string name= "Sara";
 int age =20;
 double height =1.65;
 bool isStudent=true;
+//display the output 
+Console.WriteLine("Name: "+name);
+Console.WriteLine("Age: "+age);
+Console.WriteLine("Height: "+height);
+Console.WriteLine("IsStudent: "+isStudent); 
+
+    
+    
