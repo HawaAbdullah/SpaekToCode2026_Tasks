848de14c4f92f9658f48aa9bf61c0647a45328f3
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-02 00:48:02 +0400

    Use a switch-case statement and if stetement and Print the base cost.

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -257,3 +257,34 @@ double weight = Convert.ToDouble(Console.ReadLine());
 
 double baseCost = 0;
 double extra = 0;
+switch (region)
+{
+    case 'A':
+        baseCost = 1;
+        break;
+
+    case 'B':
+        baseCost = 3;
+        break;
+
+    case 'C':
+        baseCost = 7;
+        break;
+
+    default:
+        Console.WriteLine("Invalid region");
+        return;
+}
+
+if (weight > 10)
+{
+    extra = 5;
+}
+else if (weight > 5)
+{
+    extra = 2;
+}
+
+Console.WriteLine("Base Cost: " + baseCost);
+Console.WriteLine("Extra: " + extra);
+Console.WriteLine("Total: " + (baseCost + extra));
