8dd059088873683e7083420eb4553c746e19dd50
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-07 01:49:10 +0400

    solving task 1 ,Ask the user to enter the starting number,Count down and Print the final message

---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Task 1 - Countdown Timer
+
+        // Ask the user to enter the starting number
+        Console.Write("Enter the starting number: ");
+        int startNumber = int.Parse(Console.ReadLine());
+
+        // Count down from the starting number to 1
+        for (int i = startNumber; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+
+        // Print the final message after the countdown ends
+        Console.WriteLine("Liftoff!");
+        
+    }
+}
