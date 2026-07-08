06ef7833d77af3bb3920a5860255b55083fc68bf
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-08 20:33:54 +0400

    Ask the user to enter numbers

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Task 1
+        //Ask user to enter first number
+        Console.Write("Enter The First Number: ");
+        double FirstNumber = double.Parse(Console.ReadLine());
+        
+        //Ask user to enter the secound number
+        Console.Write("Enter the Second Numver: ");
+        double SecondNumber = double.Parse(Console.ReadLine());
+    }
+}
