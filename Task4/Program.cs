69d04b6d20f685eb2de34e6fbf995342fe919001
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-10 18:43:08 +0400

    Add Task 1 - Personalized Welcome Function

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4;
+
+class Program
+{
+    static void PrintWelcome(string name)
+    {
+        Console.WriteLine("Welcome " + name + " to C# Programming!");
+    }
+    static void Main(string[] args)
+    {
+        // Ask the user for their name
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+
+        // Call the function and pass the user's name
+        PrintWelcome(userName);
+        
+        
+    }
+}
