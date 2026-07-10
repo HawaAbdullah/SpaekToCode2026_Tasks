61717317da06d16f421f3fa14cc57b8922ca0876
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-10 18:45:52 +0400

    Add Task 2 - Square Number Function with return value

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,6 +6,12 @@ class Program
     {
         Console.WriteLine("Welcome " + name + " to C# Programming!");
     }
+    // Function that calculates the square of a number
+    // It takes an integer parameter and returns an integer value
+    static int Square(int number)
+    {
+        return number * number;
+    }
     static void Main(string[] args)
     {
         // Ask the user for their name
@@ -15,6 +21,15 @@ class Program
         // Call the function and pass the user's name
         PrintWelcome(userName);
         
+        // Ask the user to enter a number
+        Console.Write("Enter a number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        // Call the function and store the returned result
+        int result = Square(userNumber);
+
+        // Print the result in Main
+        Console.WriteLine("Square = " + result);
         
     }
 }
