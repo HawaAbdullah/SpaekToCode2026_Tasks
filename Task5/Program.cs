89d93fc09dda27150200f6627cc78f76464a69bd
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-10 22:00:55 +0400

    Solve Task 2 - Dynamic To-Do List

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -24,5 +24,28 @@ class Program
         {
             Console.WriteLine(grade);
         }
+        
+        // Create a List to store to-do tasks
+        List<string> tasks = new List<string>();
+
+
+        // Ask the user to enter 5 tasks and add them to the list
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Enter task " + (i + 1) + ": ");
+            string task = Console.ReadLine();
+
+            tasks.Add(task);
+        }
+
+
+        Console.WriteLine("\nYour To-Do List:");
+
+            
+        // Print all tasks using foreach loop
+        foreach (string task in tasks)
+        {
+            Console.WriteLine("- " + task);
+        }
     }
 }
