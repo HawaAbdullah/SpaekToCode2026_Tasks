30a43d9c7c09487cccbd74b1d6e69bb8fac9b41e
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-10 21:58:40 +0400

    Solve Task 1 - Fixed Grades Array

---
+++ Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an array with fixed size of 5 integers to store grades
+        int[] grades = new int[5];
+
+
+        // Use for loop to get grades from user
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.Write("Enter grade " + (i + 1) + ": ");
+            grades[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        Console.WriteLine("\nStudent Grades:");
+
+
+        // Use foreach loop to print all grades
+        foreach (int grade in grades)
+        {
+            Console.WriteLine(grade);
+        }
+    }
+}
