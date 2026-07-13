3a714349c1c4b03191d4d84b571f48adef510c9e
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-13 21:55:53 +0400

    Add Bank acount class and add Properties and method

--- OOP_Part1_Task/Program.cs
+++ OOP_Part1_Task/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace OOP_Part1_Task;
 
+public class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public string HolderName { get; set; }
+    public double Balance { get; set; }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+        }
+        
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
     }
 }
