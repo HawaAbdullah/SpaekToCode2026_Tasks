5627a279e61e8048d610b2d2a9e37db579d3c5b0
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-13 23:04:38 +0400

    Add Student class with properties and method

--- OOP_Part1_Task/Program.cs
+++ OOP_Part1_Task/Program.cs
@@ -19,7 +19,46 @@ public class BankAccount
         }
         
     }
+
+    public double CheckBalance()
+    {
+        return Balance;
+    }
+
+    private void PrintInformation()
+    {
+        Console.WriteLine("Account number: "+AccountNumber);
+        Console.WriteLine("Holder name: "+HolderName);
+    }
+
+    private void SendEmail()
+    {
+        
+    } 
+    
 }
+public class Student
+{
+    
+    public int Grade { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    private string email { get; set; } 
+    int age {  get; set; } 
+    
+    public void Register(string Email)
+    {
+        email = Email;
+
+        SendEmail();
+    }
+
+    private void SendEmail()
+    {
+       
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
