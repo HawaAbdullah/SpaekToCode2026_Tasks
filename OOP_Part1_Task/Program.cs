ad5581116988b696bb4a2ba1e0959109a2fd0fbc
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-14 16:33:56 +0400

    Fix the code

--- OOP_Part1_Task/Program.cs
+++ OOP_Part1_Task/Program.cs
@@ -1,119 +1,120 @@
 ﻿namespace OOP_Part1_Task;
 
-public class BankAccount
-{
-    public int AccountNumber { get; set; }
-    public string HolderName { get; set; }
-    public double Balance { get; set; }
-
-    public void Deposit(double amount)
+    public class BankAccount
     {
-        Balance += amount;
-    }
+        public int AccountNumber { get; set; }
+        public string HolderName { get; set; }
+        public double Balance { get; set; }
 
-    public void Withdraw(double amount)
-    {
-        if (Balance >= amount)
+        public void Deposit(double amount)
         {
-            Balance -= amount;
-        }
-        
-    }
-
-    public double CheckBalance()
-    {
-        return Balance;
-    }
-
-    private void PrintInformation()
-    {
-        Console.WriteLine("Account number: "+AccountNumber);
-        Console.WriteLine("Holder name: "+HolderName);
-    }
-
-    private void SendEmail()
-    {
-        
-    } 
-    
-}
-public class Student
-{
-    
-    public int Grade { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    private string email { get; set; } 
-    int age {  get; set; } 
-    
-    public void Register(string Email)
-    {
-        email = Email;
-
-        SendEmail();
-    }
-
-    private void SendEmail()
-    {
-       
-    }
-}
-
-public class Product
-{
-    public string ProductName { get; set; }
-
-    public double Price { get; set; }
-
-    public int StockQuantity { get; set; }
-    
-    public void Sell(int quantity)
-    {
-        if (quantity <= StockQuantity)
-        {
-            StockQuantity -= quantity;
-            Console.WriteLine("Product sold successfully.");
-        }
-        else
-        {
-            Console.WriteLine("Not enough stock.");
+            Balance += amount;
         }
 
-        LogTransaction();
+        public void Withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+
+        }
+
+        public double CheckBalance()
+        {
+            return Balance;
+        }
+
+        private void PrintInformation()
+        {
+            Console.WriteLine("Account number: " + AccountNumber);
+            Console.WriteLine("Holder name: " + HolderName);
+        }
+
+        private void SendEmail()
+        {
+
+        }
+
     }
-    
-    public void Restock(int quantity)
+
+    public class Student
     {
-        StockQuantity += quantity;
 
-        Console.WriteLine("Product restocked successfully.");
+        public int Grade { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        private string email { get; set; }
+        int age { get; set; }
 
-        LogTransaction();
+        public void Register(string Email)
+        {
+            email = Email;
+
+            SendEmail();
+        }
+
+        private void SendEmail()
+        {
+
+        }
     }
 
-
-    
-    public double GetInventoryValue()
+    public class Product
     {
-        PrintDetails();
+        public string ProductName { get; set; }
 
-        return Price * StockQuantity;
-    }
+        public double Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public void Sell(int quantity)
+        {
+            if (quantity <= StockQuantity)
+            {
+                StockQuantity -= quantity;
+                Console.WriteLine("Product sold successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough stock.");
+            }
+
+            LogTransaction();
+        }
+
+        public void Restock(int quantity)
+        {
+            StockQuantity += quantity;
+
+            Console.WriteLine("Product restocked successfully.");
+
+            LogTransaction();
+        }
 
 
-    
-    private void PrintDetails()
-    {
-        Console.WriteLine("Product Name: " + ProductName);
-        Console.WriteLine("Price: " + Price);
-        Console.WriteLine("Stock Quantity: " + StockQuantity);
+
+        public double GetInventoryValue()
+        {
+            PrintDetails();
+
+            return Price * StockQuantity;
+        }
+
+
+
+        private void PrintDetails()
+        {
+            Console.WriteLine("Product Name: " + ProductName);
+            Console.WriteLine("Price: " + Price);
+            Console.WriteLine("Stock Quantity: " + StockQuantity);
+        }
+
+        private void LogTransaction()
+        {
+            Console.WriteLine("Transaction has been logged.");
+        }
     }
-    
-    private void LogTransaction()
-    {
-        Console.WriteLine("Transaction has been logged.");
-    }
-}
 
 
 
@@ -126,98 +127,99 @@ public class Product
 
 
 class Program
-{
-    static void Main(string[] args)
     {
-        BankAccount account1 =new BankAccount();
-        BankAccount account2 = new BankAccount();
+        BankAccount account1 = new BankAccount{AccountNumber=1163, HolderName="karim", Balance=120};
+        BankAccount account2 = new BankAccount{AccountNumber =15203, HolderName= "Ali", Balance= 63};
 
-        Student student1 = new Student();
-        Student student2 = new Student();
+        Student student1 = new Student{Name ="Ali", Address ="Muscat", Grade= 65};
+        Student student2 = new Student{Name ="Ahmed", Address= "Muscat", Grade =70};
 
-        Product product1 = new Product();
-        Product product2 = new Product();
-
-        bool exit = false;
-
-        while (!exit)
+        Product product1 = new Product{ProductName ="Wireless Mouse", Price= 5.500, StockQuantity= 50};
+        Product product2 = new Product{ProductName ="Mechanical Keyboard", Price =15.750, StockQuantity =20};
+        static void Main(string[] args)
         {
             
-            Console.WriteLine("Main Menu");
-            Console.WriteLine("1. View Account Details");
-            Console.WriteLine("2. Update Student Address");
-            Console.WriteLine("3. Make a Deposit");
-            Console.WriteLine("4. Make a Withdrawal");
-            Console.WriteLine("5. View Product Details");
-            Console.WriteLine("6. Register Student");
-            Console.WriteLine("7. Compare Account Balances");
-            Console.WriteLine("8. Restock Product");
-            Console.WriteLine("9. Transfer Between Accounts");
-            Console.WriteLine("10. Update Student Grade");
-            Console.WriteLine("11. Student Report Card");
-            Console.WriteLine("12. Account Health Status");
-            Console.WriteLine("13. Bulk Sale");
-            Console.WriteLine("14. Scholarship Eligibility");
-            Console.WriteLine("15. Full Balance Top-Up");
-            Console.WriteLine("16. Quick Account Opening");
-            Console.WriteLine("17. Total Students Counter");
-            Console.WriteLine("18. Overdrawn Account Check");
-            Console.WriteLine("19. Set Student Security PIN");
-            Console.WriteLine("20. Exit");
 
-            Console.Write("Choose an option: ");
-            int choice = int.Parse(Console.ReadLine());
+            bool exit = false;
 
-            switch (choice)
+            while (!exit)
             {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 11:
-                    break;
-                case 12:
-                    break;
-                case 13:
-                    break;
-                case 14:
-                    break;
-                case 15:
-                    break;
-                case 16:
-                    break;
-                case 17:
-                    break;
-                case 18:
-                    break;
-                case 19:
-                    break;
-                case 20:
-                    exit = true;
-                    Console.WriteLine("Thank you , have a good day!");
-                    break;
-                default:
-                    Console.WriteLine("Invalid option!");
-                    break;
+
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("1. View Account Details");
+                Console.WriteLine("2. Update Student Address");
+                Console.WriteLine("3. Make a Deposit");
+                Console.WriteLine("4. Make a Withdrawal");
+                Console.WriteLine("5. View Product Details");
+                Console.WriteLine("6. Register Student");
+                Console.WriteLine("7. Compare Account Balances");
+                Console.WriteLine("8. Restock Product");
+                Console.WriteLine("9. Transfer Between Accounts");
+                Console.WriteLine("10. Update Student Grade");
+                Console.WriteLine("11. Student Report Card");
+                Console.WriteLine("12. Account Health Status");
+                Console.WriteLine("13. Bulk Sale");
+                Console.WriteLine("14. Scholarship Eligibility");
+                Console.WriteLine("15. Full Balance Top-Up");
+                Console.WriteLine("16. Quick Account Opening");
+                Console.WriteLine("17. Total Students Counter");
+                Console.WriteLine("18. Overdrawn Account Check");
+                Console.WriteLine("19. Set Student Security PIN");
+                Console.WriteLine("20. Exit");
+
+                Console.Write("Choose an option: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
+                        break;
+                    case 19:
+                        break;
+                    case 20:
+                        exit = true;
+                        Console.WriteLine("Thank you , have a good day!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
+                }
             }
+
+
         }
-
-
     }
-}
